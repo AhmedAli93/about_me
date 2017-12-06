94f2dd6bb468d6aa298cff40bd6c8269b0e189ee
AhmedAli93 <Ahmed98148@gmail.com>
2017-12-06 09:24:11 -0800

    Almost done

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ alert('Hi there ' + name + ', I\'m going to ask you yes/no questions.');
 var work = prompt('Do you work for a living');
 var work1 = work.toUpperCase();
 console.log(work);
+
 if(work1 ==='NO'|| work1 ==='N')
 {
-var school = prompt('Do you go to school');
+var school = prompt('Are you going to school');
 var school1 = school.toUpperCase();
 console.log(school);}
 
@@ -20,10 +21,10 @@ if(school1 === 'NO' || school1 === 'N'){
 }
 
 if(work1 === 'YES' || work1 === 'Y'){
-var coding = prompt('Do you like coding');
+var coding = prompt('Have you learned how to write code?');
 var coding1 = coding.toUpperCase();
 console.log(coding);}
 
 if(coding1 === 'YES' || coding1 ==='Y'){
-  alert('Wow! I also too.');
+  alert('Wow! I have also learned how too code also.');
 }
